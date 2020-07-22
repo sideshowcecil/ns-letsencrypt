f1cde757aad01ed5c7d32e50323e245122c93380
sideshowcecil <bernd.rathmanner@gmail.com>
2020-07-22 16:43:04 +0200

    Fix an issue where only one domain was validated during dehydrated run on merges

--- letsencrypt_changes.sh
+++ letsencrypt_changes.sh
@@ -5,11 +5,11 @@ if git log -$DEPTH | grep -qE '^Merge: [0-9a-z].+ [0-9a-z].+$'; then
   BUILDTHIS=$(git diff `git log -$DEPTH | grep -E '^Merge: [0-9a-z].+ [0-9a-z].+$' | awk {'print $2,$3'}` -- domains.txt | grep -E '^\+[0-9a-z].+' | cut -d+ -f2)
   readarray -t arr < <(echo "$BUILDTHIS" | grep -vE '^$|^#')
   for domain in "${arr[@]}"; do
-    domain=$(echo ${domain} | awk {'print $1'})
+    cert=$(echo ${domain} | awk {'print $1'})
     # Fetch ssl certificates
-    ./fetch_cert.py $domain
+    ./fetch_cert.py "${cert}"
     # Run dehydrated
-    ./dehydrated/dehydrated --config ./config.sh --hook ./hook.py --cron --domain $domain
+    ./dehydrated/dehydrated --config ./config.sh --hook ./hook.py --cron --domain "${domain}"
   done
 fi
 
